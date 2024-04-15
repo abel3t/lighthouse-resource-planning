010dde9071e99af31888bce8c5255311282fffc7
Abel Tran <abeltran.develop@gmail.com>
2024-04-15 08:49:31 +0700

    feat: add clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider,  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton, } from '@clerk/nextjs'
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,14 +14,39 @@ export const metadata: Metadata = {
   description: "Lighthouse Resource Planning website",
 };
 
+function Header() {
+  return (
+    <header style={{ display: "flex", justifyContent: "space-between", padding: 20 }}>
+      <h1>My App</h1>
+      <SignedIn>
+        {/* Mount the UserButton component */}
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        {/* Signed out users get sign in button */}
+        <SignInButton/>
+      </SignedOut>
+    </header>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
+
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider>
+
+      <body className={inter.className}>
+      <Header />
+
+      {children}
+      </body>
+      </ClerkProvider>  
     </html>
+
   );
 }
