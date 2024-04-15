12844a744b3a5938432512ce88027b3d252f64ca
Abel Tran <abeltran.develop@gmail.com>
2024-04-15 12:47:52 +0700

    feat: add enums

---
+++ src/enums.ts
@@ -0,0 +1,72 @@
+export enum Role {
+  Global_Admin = 'Global_Admin',
+  Admin = 'Admin',
+  Pastor = 'Pastor',
+  Deacon = 'Deacon',
+  Staff = 'Staff',
+  Missionary = 'Missionary'
+}
+
+export enum CareType {
+  Message = 'Message',
+  FaceToFace = 'FaceToFace',
+  Call = 'Call',
+  Visit = 'Visit'
+}
+
+export enum CarePriority {
+  Good = 'Good',
+  Normal = 'Normal',
+  Warning = 'Warning'
+}
+
+export enum DateFilterSet {
+  Current,
+  TwoMonthAgo,
+  FourMonthAgo,
+  SixMonthAgo,
+  LastYear
+}
+
+export enum DiscipleType {
+  ShareGospel = 'ShareGospel',
+  Believe = 'Believe',
+  Disciple = 'Disciple'
+}
+
+export enum DisciplePriority {
+  Good = 'Good',
+  Normal = 'Normal',
+  Warning = 'Warning'
+}
+
+export enum PersonalType {
+  Member = 'Member',
+  Unbeliever = 'Unbeliever',
+  Unsure = 'Unsure',
+  Friend = 'Friend',
+  NextStep = 'NextStep',
+  NewLife = 'NewLife'
+}
+
+export enum Gender {
+  Male = 'Male',
+  Female = 'Female'
+}
+
+export enum DiscipleshipProcess {
+  Basic = 'Basic',
+  Commitment = 'Commitment',
+  Equipment = 'Equipment',
+  Empowerment = 'Empowerment'
+}
+
+export enum MaritalStatus {
+  Other = 'Other',
+  Single = 'Single',
+  Dating = 'Dating',
+  Married = 'Married',
+  Divorced = 'Divorced',
+  Widow = 'Widow',
+  Widower = 'Widower'
+}
