5fca8a7deb3336f1c5113e39974b53697248b15b
Abel Tran <abeltran.develop@gmail.com>
2024-04-20 21:41:53 +0700

    fix: query with auth

--- src/app/api/accounts/route.ts
+++ src/app/api/accounts/route.ts
@@ -2,7 +2,16 @@ import { NextResponse } from 'next/server';
 
 import prisma from '@/lib/prisma';
 
-export async function GET() {
-  const data = await prisma.account.findMany();
+export async function GET(req: Request) {
+  const organizationId = req.headers.get('x-organizationId');
+
+  if (!organizationId) {
+    return new Response('Invalid', {
+      status: 400
+    });
+  }
+
+  const data = await prisma.account.findMany({ where: { organizationId } });
+
   return NextResponse.json(data);
 }
--- src/app/api/all-members/route.ts
+++ src/app/api/all-members/route.ts
@@ -3,9 +3,17 @@ import { NextResponse } from 'next/server';
 
 import prisma from '@/lib/prisma';
 
-export async function GET() {
+export async function GET(req: Request) {
+  const organizationId = req.headers.get('x-organizationId');
+
+  if (!organizationId) {
+    return new Response('Invalid', {
+      status: 400
+    });
+  }
+
   const members = await prisma.person.findMany({
-    where: { type: PersonalType.Member },
+    where: { type: PersonalType.Member, organizationId },
     select: {
       id: true,
       name: true
