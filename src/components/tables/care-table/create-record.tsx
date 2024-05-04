8bb4212c1228d5aa3255baf55d7bac17a921f2d8
Abel Tran <abeltran.develop@gmail.com>
2024-05-04 18:12:55 +0700

    fix: update uploadthing

--- src/components/tables/care-table/create-record.tsx
+++ src/components/tables/care-table/create-record.tsx
@@ -167,6 +167,24 @@ export function CreateCareDialog() {
               {!fileUrl && (
                 <UploadButton
                   endpoint="imageUploader"
+                  content={{
+                    button({ ready }) {
+                      if (ready) return <div>{t('choose_image')}</div>;
+
+                      return t('getting_ready');
+                    },
+                    allowedContent({ ready, isUploading }) {
+                      if (!ready) {
+                        return t('wait_a_moment');
+                      }
+
+                      if (isUploading) {
+                        return t('uploading_image');
+                      }
+
+                      return t('max_image_size', { size: '8MB' });
+                    }
+                  }}
                   config={{ appendOnPaste: true }}
                   onClientUploadComplete={(res) => {
                     const file: any = res?.[0];
